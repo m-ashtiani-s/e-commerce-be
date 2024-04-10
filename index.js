5fac7b1713955baba2e5e4fe94ed9dd75d153c09
m-shtiani-s <m.ashtiani.06s@gmail.com>
2024-04-10 14:33:26 +0330

    start project

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const connectDB = require("./config/db");
+
+const app = express();
+app.use(cors());
+
+const PORT = 8000 || process.env.PORT;
+connectDB().then(() => {
+	app.listen(PORT, () => {
+		console.log("server running");
+	});
+});
